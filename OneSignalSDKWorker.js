bcdb9318c7c25f127b8bad9a0ad8125045edddf0
rifffxtt <rifathossainm51@gmail.com>
2026-03-10 15:36:21 +0600

    Create OneSignalSDKWorker.js

---
+++ OneSignalSDKWorker.js
@@ -0,0 +1,2 @@
+// Just redirect to OneSignal's hosted worker (recommended for v16 when not self-hosting custom logic)
+importScripts('https://cdn.onesignal.com/sdks/web/v16/OneSignalSDKWorker.js');
